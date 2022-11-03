7fe4eb61da2c6bd158a9e83ae7050c2969332427
Ona Narbutas <ona.narbutas@gmail.com>
2022-11-02 20:59:20 -0400

    Working time counter

--- scripts/main.js
+++ scripts/main.js
@@ -18,12 +18,14 @@ function convertToHHMM(ms) {
 function updateTime() {
   const currentTime = Date.now()
   // Update text content of display div to current time - start time
-  const timeDifferenceMS = currentTime - startTimeMS;
+  const timeDifferenceMS = currentTime - startTime;
   const timeDifferenceHHMM = convertToHHMM(timeDifferenceMS);
+
+  console.log(timeDifferenceHHMM);
 
   // Set text content of display div's time to 00:00
   const timeField = document.querySelector('.time');
-  timeField.innerText = `${timeDifferenceHHMM[hours]}:${timeDifferenceHHMM[minutes]}:${timeDifferenceHHMM[seconds]}`
+  timeField.innerText = `${timeDifferenceHHMM.hours}:${timeDifferenceHHMM.minutes}:${timeDifferenceHHMM.seconds}`
 
   // Recursive-ish call to continue updating time display every minute
   setTimeout(updateTime, 1000)
@@ -31,15 +33,15 @@ function updateTime() {
 
 // Global variables:
 let currentDomain;
+let startTime = Date.now();
 
 // when the dom is loaded:
 window.addEventListener('DOMContentLoaded', () => {
   
   // If currentDomain has changed, set start time
   if (currentDomain !== window.location.hostname) {
-      const startTimeMS = Date.now();
-      const startTimeHHMM = convertToHHMM(startTimeMS);
-
+      startTime = Date.now();
+      
       currentDomain = window.location.hostname;
   }
   
